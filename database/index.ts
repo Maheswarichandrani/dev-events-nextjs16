c982032130fe6d86ff73def9e299f08935217131
chandranimaheswari <chandranimaheswari13@gmail.com>
2025-11-24 13:57:19 +0530

    database models-2

---
+++ database/index.ts
@@ -0,0 +1,7 @@
+// Database models exports
+export { default as Event } from './eventModel';
+export { default as Booking } from './bookingModel';
+
+// TypeScript interfaces exports
+export type { IEvent } from './eventModel';
+export type { IBooking } from './bookingModel';
